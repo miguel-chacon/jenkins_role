037b461d4b3318d85ea247671e4eda966224b3dc
Miguel Chacon <mchacon@idealista.com>
2019-11-14 11:20:09 +0100

    update disable cli script

--- files/security/DisableCli.groovy
+++ files/security/DisableCli.groovy
@@ -1,4 +1,24 @@
 #!groovy
-import jenkins.model.Jenkins
+import jenkins.*
+import jenkins.model.*
+import hudson.model.*
 
-Jenkins.instance.getDescriptor("jenkins.CLI").get().setEnabled(false)
+// disabled CLI access over TCP listener (separate port)
+def p = AgentProtocol.all()
+p.each { x ->
+    if (x.name?.contains("CLI")) {
+        p.remove(x)
+    }
+}
+
+// disable CLI access over /cli URL
+def removal = { lst ->
+    lst.each { x ->
+        if (x.getClass().name.contains("CLIAction")) {
+            lst.remove(x)
+        }
+    }
+}
+def j = Jenkins.instance
+removal(j.getExtensionList(RootAction.class))
+removal(j.actions)
